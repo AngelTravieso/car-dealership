ae29b6a55e4e6d4b046a6089ad034ac7343e1302
AngelTravieso <angeltraviesoc@gmail.com>
2023-06-29 18:13:33 -0400

    Post, Patch y Delete

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
-// http://localhost/cars
+// http://localhost:3000/cars
 @Controller('cars')
 export class CarsController {
 
@@ -15,11 +15,33 @@ export class CarsController {
         return this.carsService.findAll();
     }
 
+    // ':id/:status'
     @Get(':id')
     // ParseIntPipe literalmente cambia el dato a un number
     getCarById( @Param( 'id', ParseIntPipe ) id: number ) { // Obtener queryParam
         console.log({ id });
         return this.carsService.findOneById( id )
+    }
+
+    @Post()
+    createCar( @Body() body: any ) { // capturar body
+        return body;
+    }
+
+    @Patch(':id')
+    updateCar( 
+        @Param('id', ParseIntPipe) id: number,
+        @Body() body: any ) 
+    {
+        return body;
+    }
+
+    @Delete(':id')
+    deleteCar( @Param('id', ParseIntPipe) id: number ) {
+        return {
+            method: 'DELETE',
+            id,
+        }
     }
 
 }
